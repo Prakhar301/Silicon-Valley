b91ec640a3fb1e624ceb0168551f10214ad6519f
Prakhar301 <84377317+Prakhar301@users.noreply.github.com>
2021-09-07 14:24:11 +0530

    Add files via upload

---
+++ books.js
@@ -0,0 +1,84 @@
+function back(){
+    window.location="select.html";
+}
+function Harry_Potter1(){
+    window.location="read.html";
+    localStorage.setItem("book","Harry Potter 1");
+}
+function Harry_Potter2(){
+    window.location="read.html";
+    localStorage.setItem("book","Harry Potter 2");
+}
+function Harry_Potter3(){
+    window.location="read.html";
+    localStorage.setItem("book","Harry Potter 3");
+}
+function Blue_Umbrella(){
+    window.location="read.html";
+    localStorage.setItem("book","Blue Umbrella");
+}
+function Charlie(){
+    window.location="read.html";
+    localStorage.setItem("book","Charlie and the chocolate factory");
+}
+
+function Boy(){
+    window.location="read.html";
+    localStorage.setItem("book","The Boy Next Door");
+}
+function treasure(){
+    window.location="read.html";
+    localStorage.setItem("book","Five on a Treasure Island");
+}
+function camp(){
+    window.location="read.html";
+    localStorage.setItem("book","Five go off to camp");
+}
+function island(){
+    window.location="read.html";
+    localStorage.setItem("book","The Island of Adventure");
+}
+function wood(){
+    window.location="read.html";
+    localStorage.setItem("book","The enchanted wood");
+}
+function girl(){
+    window.location="read.html";
+    localStorage.setItem("book","The naughtiest girl in the school");
+}
+function tree(){
+    window.location="read.html";
+    localStorage.setItem("book","The magic faraway tree");
+}
+function secret(){
+    window.location="read.html";
+    localStorage.setItem("book","The secret island");
+}
+function mountain(){
+    window.location="read.html";
+    localStorage.setItem("book","Mountain of adventure");
+}
+function well_done(){
+    window.location="read.html";
+    localStorage.setItem("book","Well done, secret seven");
+}
+function summer(){
+    window.location="read.html";
+    localStorage.setItem("book","Summer term at St. Clare's");
+}
+function alice(){
+    window.location="read.html";
+    localStorage.setItem("book","Alice's Adventure in Wonderland");
+}
+function anne(){
+    window.location="read.html";
+    localStorage.setItem("book","Anne of green gables");
+}
+function james(){
+    window.location="read.html";
+    localStorage.setItem("book","James and the giant peach");
+}
+function prince(){
+    window.location="read.html";
+    localStorage.setItem("book","The little prince");
+}
